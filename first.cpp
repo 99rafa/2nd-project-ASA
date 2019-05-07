4cf667601e5560c66b0ea2d9e049d4211625762f
Rafael <you@example.com>
2019-05-07 22:43:01 +0100

    semi-modified main

--- first.cpp
+++ first.cpp
@@ -6,17 +6,19 @@ using namespace std;
 
 class Network
 {
-    int numRouters
+    int numStations;
     list<int> *adjLists;
 
 public:
-    Network(int numRouters);   // Network Constructor
+    Network(int numStations);   // Network Constructor
     void addRouterToNetwork(int router1, int router2);
+    void freeNetwork();
 };
 
-    Network::Network(int numRouters)  {
+    Network::Network(int numStations, int numStoring)  {
         int i;
-        this->numRouters = numRouters;
+        this->numStations = numStations + 2;
+        adjLists = new list<int>[numRouters+];
     	}
 
     void Network::addRouterToNetwork(int router1, int router2) {
@@ -27,21 +29,29 @@ public:
     void Network::freeNetwork() {}
 
     int main() {
-        int n_routers, n_edges, router_1, router_2,i;
-        if(scanf("%d",&n_routers) < 0)
+        int n_stations,n_suppliers, supplier_production, n_storing, n_connections, router_1, router_2,i;
+        if(scanf("%d",&n_suppliers) < 0)
           exit(-1);
-        Network network(n_routers);
-
-        if(scanf("%d",&n_edges) < 0)
+        if(scanf("%d",&n_storing) < 0)
           exit(-1);
+        if(scanf("%d",&n_connections) < 0)
+          exit(-1);
+        n_stations = n_suppliers + n_storing;
+        Network network(n_stations);
 
-        for (i=0; i < n_edges; i++) {
-          if(scanf("%d %d", &router_1, &router_2) < 0)
-                exit(-1);
-            network.addRouterToNetwork(router_1, router_2);
+        list<int>::iterator r;
+        i=0;
+        for (r= adjLists[i].begin() ; r != adjLists[router_id].end(); i++) {
+            if(scanf("%d", network.[0] ) < 0)
+                  exit(-1);
+
         }
+           if(scanf("%d %d", &router_1, &router_2) < 0)
+                 exit(-1);
+             network.addRouterToNetwork(router_1, router_2);
+         }
 
         //free all the memory allocated to the network object
-        network.freeNetwork();
+        //network.freeNetwork();
         return 0;
     }
